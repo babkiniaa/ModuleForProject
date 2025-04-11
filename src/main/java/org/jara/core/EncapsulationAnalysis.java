1d25a3d663930e588604896611744794ab163626
Ярослав <jaroslaval@rambler.ru>
2025-04-11 16:59:01 +0300

    ok

---
+++ src/main/java/org/jara/core/EncapsulationAnalysis.java
@@ -0,0 +1,35 @@
+package org.jara.core;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EncapsulationAnalysis {
+
+    public List<Attentions> startAnalysis(HashMap<String, String> classes){
+         List<Attentions> attentions = new ArrayList<>();
+
+        for (Map.Entry<String, String> entry : classes.entrySet()) {
+            String fileName = entry.getKey();
+            String content = entry.getValue();
+
+            try {
+                JavaParser javaParser = new JavaParser();
+                ParseResult<CompilationUnit> parseResult = javaParser.parse(content);
+                System.out.println(parseResult.getResult().get());
+                if (parseResult.isSuccessful() && parseResult.getResult().isPresent()) {
+                    CompilationUnit cu = parseResult.getResult().get();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Ошибка при проверке инкапсуляции");
+            }
+        }
+
+        return attentions;
+    }
+}
